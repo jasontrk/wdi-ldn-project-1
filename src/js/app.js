080fc4c085eb730ce09f5224eb3a6f3ce58ae985
Jason Ranjitkumar <jasontrk@hotmail.co.uk>
2017-06-16 10:16:29 +0100

    Updated form button

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ $(() => {
   $.get(' https://newsapi.org/v1/articles?source=mtv-news-uk&sortBy=top&apiKey=77552393d57b4f59945d8062a09deaf6')
   .done((news) => {
     for(let i = 0; i < 6; i++){
+      if(!news.articles[i]) continue;
       $('.news').append(`<br><h4 href='${news.articles[i].url} 'target="_blank"<br>${news.articles[i].title}</h4><p>${news.articles[i].description}</p>`);
     }
   });
@@ -36,6 +37,15 @@ $(() => {
     // Adding the radius to the location object
     location.radius = radius;
 
+    // function hidebutton () {
+    //   if(locals.isAuthenticated){
+    //     $('.changedisplay').addClass('hidden');
+    //   } else {
+    //     $('.changedisplay').removeClass('hidden');
+    //   }
+    // }
+
+    // hidebutton();
 
     $.ajax({
       url: '/event',
@@ -45,14 +55,15 @@ $(() => {
     .then((data) => {
       data.results.forEach((event) => {
         // Ternary Operator. If comparison arguement is true then do the truthy arguement. If false, do the falsey arguement. Here I am saying, if userID is present, have hidden event details on page and add to the myevents page
-        const formHTML = userId ? `<form action="/registrations/${userId}/events" method="POST">
-        <input type="hidden" name="name" value="${event.eventname}">
-        <input type="hidden" name="venue" value="${event.venue.name}">
-        <input type="hidden" name="location" value="${event.venue.address}">
-        <input type="hidden" name="description" value="${event.description}">
-        <input type="hidden" name="category" value="${event.venue.type}">
-        <input type="submit" class="btn btn-info" value="Add to MyEvents">
-        </form>` : '';
+        const formHTML = userId ? `
+        <form action="/registrations/${userId}/events" method="POST">
+          <input type="hidden" name="name" value="${event.eventname}">
+          <input type="hidden" name="venue" value="${event.venue.name}">
+          <input type="hidden" name="location" value="${event.venue.address}">
+          <input type="hidden" name="description" value="${event.description}">
+          <input type="hidden" name="category" value="${event.venue.type}">
+          <input type="submit" class="btn btn-info" value="Add to MyEvents">
+        </form>` : '<a href="/login" id="hidebtn" class="btn btn-info changedisplay">Login to add to MyEvents</a>';
 
         console.log(event);
         $events.prepend(`
@@ -64,14 +75,12 @@ $(() => {
           <p>${event.venue.name} (${event.EventCode})</p>
           <p>${event.description}.</p>
           <a href="${event.link}" target="_blank" class="btn btn-info">More Info</a>
-          <a href="/login" class="btn btn-info">Login to add to MyEvents</a>
-
           ${formHTML}
           </div>
           </div>
           </div>
           `);
+        });
       });
-    });
-  }
-});
+    }
+  });
